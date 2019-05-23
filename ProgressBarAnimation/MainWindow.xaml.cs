377de1c19205413c24a3a40f11334b60a2b0c282
Бейсеев Санжар Бахытжанович <@corp.itstep>
2019-05-23 16:39:13 +0600

    v1

---
+++ ProgressBarAnimation/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgressBarAnimation
+{
+	/*
+	 * Создать и анимировать свой собственный progressbar.
+	 */
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			DoubleAnimation animation = new DoubleAnimation
+			{
+				From = rectangleProgress.Width,
+				To = rectangleProgress.Width + rectangleBackground.Width,
+				Duration = new Duration(TimeSpan.FromSeconds(3)),
+				RepeatBehavior = RepeatBehavior.Forever,
+				//AutoReverse = true
+			};
+
+			Storyboard storyboard = new Storyboard();
+			storyboard.Children.Add(animation);
+			Storyboard.SetTargetName(animation, rectangleProgress.Name);
+			Storyboard.SetTargetProperty(animation, new PropertyPath(Rectangle.WidthProperty));
+			storyboard.Begin(this);
+		}
+	}
+}
